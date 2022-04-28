e8446850e3cb965197494a67555ec48953e50144
Lev Litichevskiy <levlitichev@gmail.com>
2022-04-27 23:09:44 -0400

    scripts/compare_readcounts_to_imp_snps_one_chr.R: works with new imp_snp files

--- scripts/compare_readcounts_to_imp_snps_one_chr.R
+++ scripts/compare_readcounts_to_imp_snps_one_chr.R
@@ -1,68 +1,59 @@
-suppressPackageStartupMessages(library(optparse))
-suppressPackageStartupMessages(library(RSQLite))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(tools))
 
 # check if script is being run via snakemake
 if (!exists("snakemake"))
     stop(sprintf("This script only works as part of a Snakemake pipeline.")) 
 
-args <- list()
-args$readcounts_path <- "~/DO_1D_3011_148w_readcounts.csv.gz")
-args$chr <- chr <- 19
-args$sample_results_out_path <- "~/DO_1D_3011_148w_chr19_sample_results.Rds"
-args$pair_results_out_path <- "~/DO_1D_3011_148w_chr19_pair_results.Rds"
-args$imp_snp_path <- "/project/thaisslab/mbmixture/2021-01_snpcalls/with_mouse_ids/chr19_imputed_w_mouse_ids.csv.gz"
-
-#args <- list()
-#args$readcounts_path <- snakemake@input[["readcounts_path"]]
-#args$chr <- chr <- as.integer(snakemake@params[["chr"]])
-#args$sample_results_out_path <- snakemake@output[["sample_results_out_path"]]
-#args$pair_results_out_path <- snakemake@output[["pair_results_out_path"]]
-#args$snp_db_path <- snakemake@config[["sbx_mbmixture"]][["CC_sqlite_db"]]
-#args$imp_snp_dir <- snakemake@config[["sbx_mbmixture"]][["imp_snps_dir"]]
+readcounts_path <- snakemake@input[["readcounts_path"]]
+imp_snp_path <- snakemake@input[["imp_snp_path"]]
+sample <- snakemake@params[["sample"]]
+chr <- as.integer(snakemake@params[["chr"]])
+sample_results_out_path <- snakemake@output[["sample_results_out_path"]]
+pair_results_out_path <- snakemake@output[["pair_results_out_path"]]
 
 # make sure read counts file exists
-if (!file.exists(args$readcounts_path))
-    stop(sprintf("Path to read counts doesn't exist. args$readcounts_path: %s", args$readcounts_path))
+if (!file.exists(readcounts_path))
+    stop(sprintf("Path to read counts doesn't exist. readcounts_path: %s", readcounts_path))
 
 # make sure imputed SNP file exists
-if (!file.exists(args$imp_snp_path))
-    stop(sprintf("Path to imputed SNPs doesn't exist. args$imp_snp_path: %s", args$imp_snp_path))
+if (!file.exists(imp_snp_path))
+    stop(sprintf("Path to imputed SNPs doesn't exist. imp_snp_path: %s", imp_snp_path))
 
 # make sure chromosome was provided
-if (is.null(args$chr))
+if (is.null(chr))
     stop("Must provide a chromosome.")
-cat("Chromosome", args$chr, "\n")
+cat("Chromosome", chr, "\n")
 
 # make sure output paths can be written to
-if (!dir.exists(dirname(args$sample_results_out_path)))
-    stop(sprintf("Can't write to the sample_results output path. args$sample_results_out_path: %s", args$sample_results_out_path))
-if (!dir.exists(dirname(args$pair_results_out_path)))
-    stop(sprintf("Can't write to the pair_results output path. args$pair_results_out_path: %s", args$pair_results_out_path))
-
-# the readcounts file should be named like `DO_1D_3011_044w_chr19_readcounts.csv`
-# and the corresponding mouse ID is `DO-1D-3011`
-mouse.ID.for.this.mb.sample <- paste0(strsplit(basename(args$readcounts_path), "_")[[1]][1:3], collapse="-")
-cat("Mouse ID for this microbiome sample:", mouse.ID.for.this.mb.sample, "\n")
+if (!dir.exists(dirname(sample_results_out_path)))
+    stop(sprintf("Can't write to the sample_results output path. sample_results_out_path: %s", sample_results_out_path))
+if (!dir.exists(dirname(pair_results_out_path)))
+    stop(sprintf("Can't write to the pair_results output path. pair_results_out_path: %s", pair_results_out_path))
 
 # import read counts
-counts.df <- read.csv(args$readcounts_path,
+counts.df <- read.csv(readcounts_path,
                       colClasses=list(chr="character")) # keep the chromosome column as character type
-cat("Loaded", args$readcounts_path, "\n")
+cat("Loaded", readcounts_path, "\n")
 
-# import imp_snps
-cat("Loading imputed SNPs...")
-imp_snps <- read.csv(args$imp_snp_path, check.names=F)
+# the sample should be named like `DO_1D_3011_044w`
+# and the corresponding mouse ID will be `DO-1D-3011`
+mouse.ID.for.this.mb.sample <- paste0(strsplit(sample, "_")[[1]][1:3], collapse="-")
+cat("Mouse ID for this microbiome sample:", mouse.ID.for.this.mb.sample, "\n")
+
+# import imp_snps (slow)
+imp_snps <- read_csv(imp_snp_path, col_types=cols(chr="i", pos="i", .default="d"))
 mouse_IDs <- colnames(imp_snps)[3:ncol(imp_snps)]
-cat(" done.\n")
+cat("Loaded", imp_snp_path, "\n")
 
 # check that the mouse ID for our microbiome sample matches a mouse ID in imp_snps
 if (!(mouse.ID.for.this.mb.sample %in% mouse_IDs))
-  stop(sprintf("No mouse ID in imp_snps matches our mouse ID. First 3 mice in imp_snps: %s", paste0(mouse_IDs[1:3], collapse=", ")))
+  stop(sprintf("No mouse ID in imp_snps matches our mouse ID. Mouse ID in microbiome sample: %s, first 3 mice in imp_snps: %s", mouse.ID.for.this.mb.sample, paste0(mouse_IDs[1:3], collapse=", ")))
 
-# counts.df should be a superset of SNPs that could be imputed
+# SNPs in counts.df should be a subset of imputed bi-allelic SNPs
+# (because in get_readcounts.R, we already subset to bi-allelic SNPs in the CC sqlite db)
 merged  <- merge(imp_snps, counts.df, by=c("chr", "pos"))
 stopifnot(nrow(merged) > 0)
 cat("Using", nrow(merged), "SNPs to compare this microbiome sample to genotyped mice...\n")
@@ -95,7 +86,7 @@ dimnames(pair_results) <- list(mouse_IDs, c("AA", "AB", "BB"),
 
 # loop over mice
 for (ii in 1:length(mouse_IDs)) {
-  
+  cat("Comparing microbiome sample to mouse", ii, "of", length(mouse_IDs), "\n") 
   this.mouse <- mouse_IDs[ii]
   
   # SAMPLE_RESULTS
@@ -113,32 +104,24 @@ for (ii in 1:length(mouse_IDs)) {
   sample_results[ii,,] <- sample.result.for.ii
   
   # PAIR_RESULTS
-  # add the expected mouse genotype
-  expected.mouse.genotype.df <- t(imp_snps[mouse.ID.for.this.mb.sample, ])
-  colnames(expected.mouse.genotype.df) <- "expected.genotype"
-  counts.df.for.ii <- merge(
-    counts.df.for.ii, expected.mouse.genotype.df,
-    by.x="snp_id", by.y="row.names")
-  
   # count up the number of reads for major and minor alleles for every combination of
   # expected genotype versus all other genotypes
-  pair.result.for.ii <- counts.df.for.ii %>%
-    mutate(this.genotype=factor(this.genotype, levels=c(1,2,3)),
-           expected.genotype=factor(expected.genotype, levels=c(1,2,3))) %>% 
+  pair.result.for.ii <- merged %>%
+    mutate(this.genotype=factor(.data[[this.mouse]], levels=c(0,0.5,1)),
+           expected.genotype=factor(.data[[mouse.ID.for.this.mb.sample]], levels=c(0,0.5,1))) %>%
+    dplyr::filter(!is.na(this.genotype)) %>%
+    dplyr::filter(!is.na(expected.genotype)) %>%
     group_by(this.genotype, expected.genotype, .drop=F) %>%
     summarise(A=sum(count1), B=sum(count2), .groups="drop") 
   pair_results[ii,,,1] <- pair.result.for.ii  %>%
     pivot_wider(id_cols=expected.genotype, names_from=this.genotype, values_from=A) %>% 
-    dplyr::select(c(`1`,`2`,`3`)) %>% mutate_all(function(x) as.integer(x)) %>% as.matrix
+    dplyr::select(c(`0`,`0.5`,`1`)) %>% mutate_all(function(x) as.integer(x)) %>% as.matrix
   pair_results[ii,,,2] <- pair.result.for.ii  %>%
     pivot_wider(id_cols=expected.genotype, names_from=this.genotype, values_from=B) %>%
-    dplyr::select(c(`1`,`2`,`3`)) %>% mutate_all(function(x) as.integer(x)) %>% as.matrix
+    dplyr::select(c(`0`,`0.5`,`1`)) %>% mutate_all(function(x) as.integer(x)) %>% as.matrix
 }  
 
 # write output for this chromosome  
-saveRDS(sample_results, args$sample_results_out_path)
-saveRDS(pair_results, args$pair_results_out_path)
-cat("Saved results for chr", chr, ".\n")
-
-# disconnect from CC database
-dbDisconnect(snp.db)
+saveRDS(sample_results, sample_results_out_path)
+saveRDS(pair_results, pair_results_out_path)
+cat("Saved results for chr", chr, "\n")
